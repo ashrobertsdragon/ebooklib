0840ce8dcf7a02ecaeeb55d0d0ab4441ae646e20
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-06-23 19:19:07 -0600

    Remove additional trailing line at end of setup.py

--- setup.py
+++ setup.py
@@ -12,18 +12,18 @@ def read(path):
 
 
 setup(
-    name = 'EbookLib',
-    version = '0.18',
-    author = 'Aleksandar Erkalovic',
-    author_email = 'aerkalov@gmail.com',
-    packages = ['ebooklib', 'ebooklib.plugins'],
-    url = 'https://github.com/aerkalov/ebooklib',
-    license = 'GNU Affero General Public License',
-    description = 'Ebook library which can handle EPUB2/EPUB3 and Kindle format',
-    long_description = read('README.md'),
-    long_description_content_type = 'text/markdown',
-    keywords = ['ebook', 'epub', 'kindle'],
-    classifiers = [
+    name="EbookLib",
+    version="0.18",
+    author="Aleksandar Erkalovic",
+    author_email="aerkalov@gmail.com",
+    packages=["ebooklib", "ebooklib.plugins"],
+    url="https://github.com/aerkalov/ebooklib",
+    license="GNU Affero General Public License",
+    description="Ebook library which can handle EPUB2/EPUB3 and Kindle format",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    keywords=["ebook", "epub", "kindle"],
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
@@ -32,10 +32,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Topic :: Software Development :: Libraries :: Python Modules"
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    install_requires = [
-       "lxml", "six"
-    ]
+    install_requires=["lxml", "six"],
 )
-
